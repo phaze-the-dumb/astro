1cf1dde5dfc437917b099be40e5f5fd666d35129
phaze-the-dumb <phaze@duck.com>
2023-10-15 12:39:14 +0100

    Finish backend commenting

--- src/index.js
+++ src/index.js
@@ -21,29 +21,37 @@ if(!fs.existsSync(path.join(os.homedir(), './AppData/Roaming/PhazeDev/.config/as
 let config = new Config(path.join(os.homedir(), './AppData/Roaming/PhazeDev/.config/astro.json'));
 server.config(config);
 
+// Init empty values
 let slideTimeout = null;
 let currentSlideIndex = 0;
 let currentUrl = 'http://localhost';
 
+// Create a new adblocking filter
 const filterSet = new adblockRust.FilterSet(false);
 
+// Load the easylist.txt filter
 let easylistFilters = fs.readFileSync(__dirname + '/data/easylist.txt', { encoding: 'utf-8' }).split('\n');
 filterSet.addFilters(easylistFilters);
 
+// Load the unbreak.txt filter
 let uboUnbreakFilters = fs.readFileSync(__dirname + '/data/unbreak.txt', { encoding: 'utf-8' }).split('\n');
 filterSet.addFilters(uboUnbreakFilters);
 
+// Load the uBlock Origin files
 const resources = adblockRust.uBlockResources(
   __dirname + '/data/fake-uBO-files/web_accessible_resources',
   __dirname + '/data/fake-uBO-files/redirect-resources.js',
   __dirname + '/data/fake-uBO-files/scriptlets.js'
 );
 
+// Create an adblocking engine
 const engine = new adblockRust.Engine(filterSet, false);
 engine.useResources(resources);
 
 app.on('ready', () => {
+  // When the app is ready, hook into electrons webrequest module
   session.defaultSession.webRequest.onBeforeRequest({ urls: [ '*://*/*' ] }, ( details, callback ) => {
+    // Filter the urls so ads do not load
     if(engine.check(details.url, currentUrl, 'script')){
       callback({ cancel: true });
     } else{
@@ -51,6 +59,7 @@ app.on('ready', () => {
     }
   })
 
+  // Create the main window
   const mainWindow = new BrowserWindow({
     fullscreen: true,
     webPreferences: {
@@ -60,32 +69,41 @@ app.on('ready', () => {
     }
   });
 
+  // Hide the windows menu bar
   mainWindow.setMenuBarVisibility(false);
 
+  // If autostart is enabled and we have slides, start the slideshow
   if(config.autoStart && config.slides.length > 0){
     displaySlide(mainWindow);
   } else{
+    // If not load the landing page
+
     if(isDev = process.env.APP_DEV ? (process.env.APP_DEV.trim() == "true") : false)
       mainWindow.loadURL('http://localhost:5173/');
     else
       mainWindow.loadFile(path.join(__dirname, '../ui/index.html'));
   }
 
+  // Load the applications icon
   let icon = nativeImage.createFromPath(path.join(__dirname, '../build/icon.ico'));
   mainWindow.setIcon(icon);
 
+  // Hook the link code event, and relay it to the window contents
   server.getEmitter().on('link-code', ( code ) => {
     mainWindow.webContents.send('link-code', code);
   })
 
+  // Hook the linked event, and relay it to the window contents
   server.getEmitter().on('linked', () => {
     mainWindow.webContents.send('linked');
   })
 
+  // Hook the slides update event, and relay it to the window contents
   server.getEmitter().on('slides-update', ( type, slide ) => {
     mainWindow.webContents.send('slides-update', type, slide);
   })
 
+  // Hook the start event, and start the slideshow
   server.getEmitter().on('start', () => {
     mainWindow.webContents.send('unload');
     currentSlideIndex = 0;
@@ -95,16 +113,19 @@ app.on('ready', () => {
     }, 100);
   })
 
+  // Hook the stop event, and stop the slideshow
   server.getEmitter().on('stop', () => {
     clearTimeout(slideTimeout);
     currentUrl = 'http://localhost';
 
+    // Load the landing page
     if(isDev = process.env.APP_DEV ? (process.env.APP_DEV.trim() == "true") : false)
       mainWindow.loadURL('http://localhost:5173/');
     else
       mainWindow.loadFile(path.join(__dirname, '../ui/index.html'));
   })
 
+  // Hook the next slide evemt and go to the next slide
   server.getEmitter().on('next-slide', ( cb ) => {
     clearTimeout(slideTimeout);
 
@@ -119,6 +140,7 @@ app.on('ready', () => {
     cb();
   });
 
+  // Hook the prev slide event and go to the previous slide
   server.getEmitter().on('prev-slide', ( cb ) => {
     clearTimeout(slideTimeout);
 
@@ -133,15 +155,18 @@ app.on('ready', () => {
     cb();
   });
 
+  // When the window contents requests the config file, send the config file
   ipcMain.on('getConfig', () => {
     mainWindow.webContents.send('getConfig', config);
   });
 });
 
 let displaySlide = ( win ) => {
+  // Call the slide change event
   server.getEmitter().emit('slide-change', currentSlideIndex);
   let currentSlide = config.slides[currentSlideIndex];
 
+  // Check what type of slide it is, if it's a website, load the website
   switch(currentSlide.type) {
     case 1:
       currentUrl = currentSlide.url;
@@ -149,9 +174,11 @@ let displaySlide = ( win ) => {
       break;
   }
 
+  // Wait for the time to run out
   slideTimeout = setTimeout(() => {
     if(!server.getActive())return;
 
+    // Go to the next slide
     if(config.slides[currentSlideIndex + 1]){
       currentSlideIndex += 1;
       displaySlide(win);
